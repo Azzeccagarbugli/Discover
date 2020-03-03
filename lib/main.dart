9838476e3a487dcf213fdd2510c248dab5e9e7d9
azzeccagarbugli <f.coppola1998@gmail.com>
2020-03-03 01:36:36 +0100

    [UPDATE] Aggiunte modifiche estetiche all'applicazione

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_circular_chart/flutter_circular_chart.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:noise_meter/noise_meter.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,6 +37,8 @@ class _MyHomePageState extends State<MyHomePage> {
   NoiseMeter _noiseMeter;
 
   double _noiseDB = 0;
+  double _maxNoiseDB = 0;
+  double _minNoiseDB = 0;
 
   int _selectedIndex = 0;
 
@@ -80,14 +84,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _cycleSamples() {
     List<CircularStackEntry> nextData = <CircularStackEntry>[
-      _buildGraph(_noiseDB),
+      _buildGraphMinNoise(_minNoiseDB),
+      _buildGraphMaxNoise(_maxNoiseDB),
+      _buildGraphActualNoise(_noiseDB),
     ];
+
     setState(() {
+      if (_noiseDB > _maxNoiseDB) {
+        _maxNoiseDB = _noiseDB;
+      }
+      if (_minNoiseDB > _noiseDB) {
+        _minNoiseDB = _noiseDB;
+      }
       _chartKey.currentState.updateData(nextData);
     });
   }
 
-  CircularStackEntry _buildGraph(double value) {
+  CircularStackEntry _buildGraphActualNoise(double value) {
     return CircularStackEntry(
       <CircularSegmentEntry>[
         new CircularSegmentEntry(
@@ -105,116 +118,186 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  CircularStackEntry _buildGraphMaxNoise(double value) {
+    return CircularStackEntry(
+      <CircularSegmentEntry>[
+        new CircularSegmentEntry(
+          value,
+          Colors.red[800],
+          rankKey: 'completed',
+        ),
+        new CircularSegmentEntry(
+          (100 - value),
+          Colors.red[50],
+          rankKey: 'remaining',
+        ),
+      ],
+      rankKey: 'progress',
+    );
+  }
+
+  CircularStackEntry _buildGraphMinNoise(double value) {
+    return CircularStackEntry(
+      <CircularSegmentEntry>[
+        new CircularSegmentEntry(
+          value,
+          Colors.green[800],
+          rankKey: 'completed',
+        ),
+        new CircularSegmentEntry(
+          (100 - value),
+          Colors.green[50],
+          rankKey: 'remaining',
+        ),
+      ],
+      rankKey: 'progress',
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     if (_isRecording) {
       _cycleSamples();
     }
+
+    onBottom(Widget child) => Positioned.fill(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: child,
+          ),
+        );
+
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: <Widget>[
-            Center(
+      body: Stack(
+        children: <Widget>[
+          Positioned.fill(child: AnimatedBackground()),
+          onBottom(AnimatedWave(
+            height: 180,
+            speed: 1.0,
+          )),
+          onBottom(AnimatedWave(
+            height: 120,
+            speed: 0.9,
+            offset: pi,
+          )),
+          onBottom(AnimatedWave(
+            height: 220,
+            speed: 1.2,
+            offset: pi / 2,
+          )),
+          Center(
+            child: Container(
+              margin: const EdgeInsets.all(52),
+              decoration: new BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey[500],
+                    offset: Offset(4.0, 4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0,
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-4.0, -4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0,
+                  ),
+                ],
+              ),
               child: AnimatedCircularChart(
                 key: _chartKey,
                 size: Size(350, 350),
                 initialChartData: <CircularStackEntry>[
-                  _buildGraph(_noiseDB),
+                  _buildGraphMinNoise(_minNoiseDB),
+                  _buildGraphMaxNoise(_maxNoiseDB),
+                  _buildGraphActualNoise(_noiseDB),
                 ],
                 chartType: CircularChartType.Radial,
                 edgeStyle: SegmentEdgeStyle.round,
                 percentageValues: true,
-                holeLabel: _noiseDB.truncate().toString() + " db",
-                labelStyle: GoogleFonts.quicksand(
-                  textStyle: TextStyle(
-                    color: Colors.blue[800],
-                    fontSize: 62,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
+                // holeLabel: _noiseDB.truncate().toString() + " db",
+                // labelStyle: GoogleFonts.quicksand(
+                //   textStyle: TextStyle(
+                //     color: Colors.blue[800],
+                //     fontSize: 42,
+                //     fontWeight: FontWeight.w700,
+                //   ),
+                // ),
               ),
             ),
-            Positioned(
-              bottom: 36,
-              left: 18,
-              right: 18,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: new BorderRadius.circular(62),
-                  boxShadow: [
-                    BoxShadow(
-                      spreadRadius: -10,
-                      blurRadius: 60,
-                      color: Colors.black.withOpacity(.20),
-                      offset: Offset(0, 15),
-                    )
-                  ],
+          ),
+          Positioned(
+            bottom: 36,
+            left: 24,
+            right: 24,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: new BorderRadius.circular(62),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: -10,
+                    blurRadius: 60,
+                    color: Colors.black.withOpacity(.20),
+                    offset: Offset(0, 15),
+                  )
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 18.0,
+                  vertical: 10,
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 18.0,
-                    vertical: 10,
-                  ),
-                  child: GNav(
-                      gap: 8,
-                      color: Colors.grey[800],
-                      activeColor: Colors.blue[800],
-                      iconSize: 24,
-                      // backgroundColor: Colors.black,
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 5,
+                child: GNav(
+                    gap: 8,
+                    textStyle: GoogleFonts.quicksand(
+                      textStyle: TextStyle(
+                        color: Colors.blue[800],
+                        fontWeight: FontWeight.w800,
                       ),
-                      duration: Duration(milliseconds: 800),
-                      tabs: [
-                        GButton(
-                          active: true,
-                          icon: Icons.hearing,
-                          text: 'Listen',
-                          textStyle: GoogleFonts.quicksand(
-                            textStyle: TextStyle(
-                              color: Colors.blue[800],
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          backgroundColor: Colors.blue[50],
-                        ),
-                        GButton(
-                          icon: Icons.library_books,
-                          text: 'Records',
-                          textStyle: GoogleFonts.quicksand(
-                            textStyle: TextStyle(
-                              color: Colors.blue[800],
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          backgroundColor: Colors.blue[50],
-                        ),
-                        GButton(
-                          icon: Icons.favorite,
-                          text: 'Search',
-                          textStyle: GoogleFonts.quicksand(
-                            textStyle: TextStyle(
-                              color: Colors.blue[800],
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          backgroundColor: Colors.blue[50],
-                        ),
-                      ],
-                      selectedIndex: _selectedIndex,
-                      onTabChange: (index) {
-                        print(index);
-                        setState(() {
-                          _selectedIndex = index;
-                        });
-                      }),
-                ),
+                    ),
+                    color: Colors.grey[800],
+                    activeColor: Colors.blue[800],
+                    iconSize: 24,
+                    // backgroundColor: Colors.black,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    duration: Duration(
+                      milliseconds: 800,
+                    ),
+                    tabs: [
+                      GButton(
+                        active: true,
+                        icon: Icons.hearing,
+                        text: 'Listen',
+                        backgroundColor: Colors.blue[50],
+                      ),
+                      GButton(
+                        icon: Icons.library_books,
+                        text: 'Records',
+                        backgroundColor: Colors.blue[50],
+                      ),
+                      GButton(
+                        icon: Icons.favorite,
+                        text: 'Search',
+                        backgroundColor: Colors.blue[50],
+                      ),
+                    ],
+                    selectedIndex: _selectedIndex,
+                    onTabChange: (index) {
+                      print(index);
+                      setState(() {
+                        _selectedIndex = index;
+                      });
+                    }),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue[800],
@@ -229,5 +312,92 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+}
+
+class AnimatedBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTrackTween([
+      Track("color1").add(Duration(seconds: 3),
+          ColorTween(begin: Color(0xffD38312), end: Colors.lightBlue.shade900)),
+      Track("color2").add(Duration(seconds: 3),
+          ColorTween(begin: Color(0xffA83279), end: Colors.blue.shade600))
+    ]);
+
+    return ControlledAnimation(
+      playback: Playback.MIRROR,
+      tween: tween,
+      duration: tween.duration,
+      builder: (context, animation) {
+        return Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [animation["color1"], animation["color2"]])),
+        );
+      },
+    );
+  }
+}
+
+class AnimatedWave extends StatelessWidget {
+  final double height;
+  final double speed;
+  final double offset;
+
+  AnimatedWave({this.height, this.speed, this.offset = 0.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return Container(
+        height: height,
+        width: constraints.biggest.width,
+        child: ControlledAnimation(
+            playback: Playback.LOOP,
+            duration: Duration(milliseconds: (5000 / speed).round()),
+            tween: Tween(begin: 0.0, end: 2 * pi),
+            builder: (context, value) {
+              return CustomPaint(
+                foregroundPainter: CurvePainter(value + offset),
+              );
+            }),
+      );
+    });
+  }
+}
+
+class CurvePainter extends CustomPainter {
+  final double value;
+
+  CurvePainter(this.value);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final white = Paint()..color = Colors.white.withAlpha(60);
+    final path = Path();
+
+    final y1 = sin(value);
+    final y2 = sin(value + pi / 2);
+    final y3 = sin(value + pi);
+
+    final startPointY = size.height * (0.5 + 0.4 * y1);
+    final controlPointY = size.height * (0.5 + 0.4 * y2);
+    final endPointY = size.height * (0.5 + 0.4 * y3);
+
+    path.moveTo(size.width * 0, startPointY);
+    path.quadraticBezierTo(
+        size.width * 0.5, controlPointY, size.width, endPointY);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    canvas.drawPath(path, white);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
