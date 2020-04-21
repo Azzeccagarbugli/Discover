b62a550a0d22064736f9b46c91b37f5a60400afb
azzeccagarbugli <f.coppola1998@gmail.com>
2020-04-21 02:04:21 +0200

    [UPDATE] Impostata nuova logica visiva per la registrazione dei suoni

--- lib/ui/widgets/flip_card/flip_card_back_page.dart
+++ lib/ui/widgets/flip_card/flip_card_back_page.dart
@@ -1,16 +1,20 @@
 import 'package:Discover/models/track.dart';
 import 'package:Discover/ui/widgets/effects/neumorphism.dart';
 import 'package:flutter/material.dart';
+import 'package:spring_button/spring_button.dart';
 import 'package:theme_provider/theme_provider.dart';
 
 class BackFlipCard extends StatefulWidget {
   const BackFlipCard({
     Key key,
     @required double value,
+    @required bool isRecording,
   })  : _value = value,
+        _isRecording = isRecording,
         super(key: key);
 
   final double _value;
+  final bool _isRecording;
 
   @override
   _BackFlipCardState createState() => _BackFlipCardState();
@@ -19,10 +23,17 @@ class BackFlipCard extends StatefulWidget {
 class _BackFlipCardState extends State<BackFlipCard> {
   List<Sound> _lst = new List<Sound>();
 
+  bool _isSaving = false;
+
   @override
   Widget build(BuildContext context) {
-    _lst.add(Sound(widget._value));
-    print(_lst.toString());
+    // _lst.add(Sound(widget._value));
+    // print(_lst.toString());
+
+    if (!widget._isRecording) {
+      _isSaving = false;
+    }
+
     return Stack(
       children: <Widget>[
         Center(
@@ -39,10 +50,36 @@ class _BackFlipCardState extends State<BackFlipCard> {
             ),
             child: Center(
               child: ListTile(
-                leading: Icon(
-                  Icons.radio_button_checked,
-                  color: Colors.red,
-                  size: 56,
+                onTap: () {
+                  print("object");
+                },
+                leading: SpringButton(
+                  SpringButtonType.OnlyScale,
+                  Container(
+                    decoration: new BoxDecoration(
+                      color: ThemeProvider.themeOf(context)
+                          .data
+                          .scaffoldBackgroundColor,
+                      shape: BoxShape.circle,
+                      boxShadow: Neumorphism.boxShadow(context),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(18, 8, 18, 8),
+                      child: Icon(
+                        Icons.graphic_eq,
+                        color: this.widget._isRecording
+                            ? (_isSaving ? Colors.green[400] : Colors.orange)
+                            : Colors.grey[400],
+                        size: 42,
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _isSaving = !_isSaving;
+                    });
+                  },
+                  useCache: false,
                 ),
                 title: Text(
                   "RECORD",
@@ -53,11 +90,17 @@ class _BackFlipCardState extends State<BackFlipCard> {
                   ),
                 ),
                 subtitle: Text(
-                  "Save the current session of sounds",
+                  "Tap to save the current session of sounds",
                   style: TextStyle(
                     color: Colors.grey,
-                    fontSize: 18,
                   ),
+                  maxLines: 2,
+                  overflow: TextOverflow.fade,
+                ),
+                trailing: Icon(
+                  Icons.brightness_1,
+                  color: Colors.grey[400],
+                  size: 12,
                 ),
               ),
             ),
