cb67e2f0e8dce9e709cfd5fb4de906159eabf6f0
azzeccagarbugli <f.coppola1998@gmail.com>
2020-05-03 02:48:29 +0200

    [UPDATE] Aggiunto widget per il not found stuff

--- lib/ui/views/tracks_page.dart
+++ lib/ui/views/tracks_page.dart
@@ -1,9 +1,10 @@
 import 'package:Discover/main.dart';
 import 'package:Discover/models/track.dart';
+import 'package:Discover/ui/widgets/effects/remove_glow_listview.dart';
 import 'package:Discover/ui/widgets/lateral_action.dart';
+import 'package:Discover/ui/widgets/not_found.dart';
 import 'package:Discover/ui/widgets/track_item_list.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -23,141 +24,91 @@ class _TracksViewState extends State<TracksView> {
       backgroundColor: ThemeProvider.themeOf(context).id == "light_theme"
           ? Colors.white
           : Colors.grey[900],
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: ClipPath(
-              clipper: OvalBottomBorderClipper(),
-              child: Container(
-                color: Colors.blue[50],
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Expanded(
-                      child: Stack(
-                        children: <Widget>[
-                          Align(
-                            alignment: Alignment.topRight,
-                            child: Image.asset(
-                              "assets/images/tracks.png",
-                            ),
-                          ),
-                          Positioned(
-                            top: 40,
-                            left: 20,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  "Tracks",
-                                  style: ThemeProvider.themeOf(context)
-                                      .data
-                                      .primaryTextTheme
-                                      .title
-                                      .copyWith(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 42,
-                                      ),
-                                ),
-                                Text(
-                                  "Feel what \nyou lived",
-                                  style: ThemeProvider.themeOf(context)
-                                      .data
-                                      .primaryTextTheme
-                                      .body1
-                                      .copyWith(
-                                        fontSize: 26,
-                                      ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 5,
-            child: ValueListenableBuilder(
-              valueListenable:
-                  Hive.box<Track>(Discover.trackBoxName).listenable(),
-              builder: (context, Box<Track> tracks, _) {
-                return ListView.separated(
-                  key: _myList,
-                  separatorBuilder: (_, index) => Divider(),
-                  itemCount: tracks.keys.cast<int>().toList().length,
-                  itemBuilder: (_, index) {
-                    final int key = tracks.keys
-                        .cast<int>()
-                        .toList()
-                        .reversed
-                        .toList()[index];
+      body: Padding(
+        padding: const EdgeInsets.only(top: 16),
+        child: ValueListenableBuilder(
+          valueListenable: Hive.box<Track>(Discover.trackBoxName).listenable(),
+          builder: (context, Box<Track> tracks, _) {
+            if (tracks.isEmpty) {
+              return NotFound(
+                pathImg: "assets/images/tracks.png",
+                title: "Any track found!",
+                subtitile:
+                    "If you would like to add a new track just go to the homepage and start to record a new session of sounds",
+              );
+            }
 
-                    final Track trk = tracks.get(key);
+            return ScrollConfiguration(
+              behavior: RemoveGlow(),
+              child: ListView.separated(
+                key: _myList,
+                separatorBuilder: (_, index) => Divider(),
+                itemCount: tracks.keys.cast<int>().toList().length,
+                itemBuilder: (_, index) {
+                  final int key =
+                      tracks.keys.cast<int>().toList().reversed.toList()[index];
 
-                    return Slidable(
-                      key: Key(key.toString()),
-                      actionPane: SlidableDrawerActionPane(),
-                      closeOnScroll: true,
-                      showAllActionsThreshold: 0.5,
-                      dismissal: SlidableDismissal(
-                        child: SlidableDrawerDismissal(),
-                        dismissThresholds: <SlideActionType, double>{
-                          SlideActionType.secondary: 1.0
-                        },
-                        onDismissed: (actionType) {
+                  final Track trk = tracks.get(key);
+
+                  return Slidable(
+                    key: Key(key.toString()),
+                    actionPane: SlidableDrawerActionPane(),
+                    closeOnScroll: true,
+                    showAllActionsThreshold: 0.5,
+                    dismissal: SlidableDismissal(
+                      child: SlidableDrawerDismissal(),
+                      dismissThresholds: <SlideActionType, double>{
+                        SlideActionType.secondary: 1.0
+                      },
+                      onDismissed: (actionType) {
+                        setState(() {
                           tracks.delete(key);
-                        },
+                        });
+                      },
+                    ),
+                    actions: <Widget>[
+                      LateralAction(
+                        closeOnTap: true,
+                        icon: Icons.delete,
+                        pos: key,
+                        onTap: () {},
+                        color: Colors.red,
                       ),
-                      actions: <Widget>[
-                        LateralAction(
-                          closeOnTap: true,
-                          icon: Icons.delete,
-                          pos: key,
-                          onTap: () {},
-                          color: Colors.red,
-                        ),
-                      ],
-                      secondaryActions: <Widget>[
-                        LateralAction(
-                          closeOnTap: false,
-                          trk: trk,
-                          pos: key,
-                          onTap: () {
-                            Track nTrack = new Track(
-                              sound: trk.sound,
-                              date: trk.date,
-                              isSaved: !trk.isSaved,
-                            );
-
-                            tracks.put(key, nTrack);
-                          },
-                          color: Colors.green,
-                          tracks: tracks,
-                        ),
-                        LateralAction(
-                          closeOnTap: true,
-                          icon: Icons.share,
-                          pos: key,
-                          onTap: () {},
-                          color: Colors.indigoAccent,
-                        ),
-                      ],
-                      child: TrackItemList(
+                    ],
+                    secondaryActions: <Widget>[
+                      LateralAction(
+                        closeOnTap: false,
                         trk: trk,
+                        pos: key,
+                        onTap: () {
+                          Track nTrack = new Track(
+                            sound: trk.sound,
+                            date: trk.date,
+                            isSaved: !trk.isSaved,
+                          );
+
+                          tracks.put(key, nTrack);
+                        },
+                        color: Colors.green,
+                        tracks: tracks,
                       ),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-        ],
+                      LateralAction(
+                        closeOnTap: true,
+                        icon: Icons.share,
+                        pos: key,
+                        onTap: () {},
+                        color: Colors.blue[600],
+                      ),
+                    ],
+                    child: TrackItemList(
+                      trk: trk,
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
---
+++ lib/ui/widgets/not_found.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:theme_provider/theme_provider.dart';
+
+class NotFound extends StatelessWidget {
+  final String pathImg;
+  final String title;
+  final String subtitile;
+
+  const NotFound({
+    @required this.pathImg,
+    @required this.title,
+    @required this.subtitile,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            pathImg,
+            scale: 4,
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Text(
+            title,
+            style: ThemeProvider.themeOf(context)
+                .data
+                .primaryTextTheme
+                .title
+                .copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 36,
+                ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 22,
+              vertical: 6,
+            ),
+            child: Text(
+              subtitile,
+              textAlign: TextAlign.center,
+              style: ThemeProvider.themeOf(context)
+                  .data
+                  .primaryTextTheme
+                  .body1
+                  .copyWith(
+                    fontSize: 16,
+                  ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
