8dcc7cf9b7ca9fb89e892264127a047dcfd0f2d1
azzeccagarbugli <f.coppola1998@gmail.com>
2020-05-31 21:14:31 +0200

    [UPDATE] Inserite animazioni nella pagina relativa al recorder

--- lib/ui/widgets/flip_card/flip_card_back_page.dart
+++ lib/ui/widgets/flip_card/flip_card_back_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:Discover/main.dart';
 import 'package:Discover/models/track.dart';
 import 'package:Discover/ui/widgets/effects/glowicon.dart';
@@ -26,24 +28,52 @@ class BackFlipCard extends StatefulWidget {
   _BackFlipCardState createState() => _BackFlipCardState();
 }
 
-class _BackFlipCardState extends State<BackFlipCard> {
+class _BackFlipCardState extends State<BackFlipCard>
+    with TickerProviderStateMixin {
   bool _isSaving = false;
 
   List<double> _values;
 
   Box<Track> _trackBox;
 
+  AnimationController _rotationControllerFaster;
+  AnimationController _rotationControllerSlower;
+
   @override
   void initState() {
     super.initState();
     _values = new List<double>();
     _trackBox = Hive.box<Track>(Discover.trackBoxName);
+    _rotationControllerFaster = AnimationController(
+      duration: const Duration(
+        seconds: 18,
+      ),
+      vsync: this,
+    );
+    _rotationControllerSlower = AnimationController(
+      duration: const Duration(
+        seconds: 15,
+      ),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _rotationControllerFaster.dispose();
+    _rotationControllerSlower.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     if (!widget._isRecording) {
       _isSaving = false;
+      _rotationControllerFaster.stop();
+      _rotationControllerSlower.stop();
+    } else {
+      _rotationControllerFaster.repeat();
+      _rotationControllerSlower.repeat();
     }
 
     if (_isSaving && widget._isRecording) {
@@ -65,64 +95,83 @@ class _BackFlipCardState extends State<BackFlipCard> {
               boxShadow: Neumorphism.boxShadow(context),
             ),
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CircularText(
-                children: [
-                  TextItem(
-                    text: Text(
-                      "RECORD".toUpperCase(),
-                      style: ThemeProvider.themeOf(context)
-                          .data
-                          .primaryTextTheme
-                          .headline6
-                          .copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                    space: 12,
-                    startAngle: 0,
-                    startAngleAlignment: StartAngleAlignment.center,
-                    direction: CircularTextDirection.clockwise,
-                  ),
-                  TextItem(
-                    text: Text(
-                      "RECORD".toUpperCase(),
-                      style: ThemeProvider.themeOf(context)
-                          .data
-                          .primaryTextTheme
-                          .headline6
-                          .copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                    space: 12,
-                    startAngle: 120,
-                    startAngleAlignment: StartAngleAlignment.center,
-                    direction: CircularTextDirection.clockwise,
-                  ),
-                  TextItem(
-                    text: Text(
-                      "RECORD".toUpperCase(),
-                      style: ThemeProvider.themeOf(context)
-                          .data
-                          .primaryTextTheme
-                          .headline6
-                          .copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                    space: 12,
-                    startAngle: -120,
-                    startAngleAlignment: StartAngleAlignment.center,
-                    direction: CircularTextDirection.clockwise,
-                  ),
-                ],
+              padding: const EdgeInsets.all(12.0),
+              child: RotationTransition(
+                turns: Tween(begin: 0.0, end: 1.0)
+                    .animate(_rotationControllerFaster),
+                child: BuildCircularText(
+                  radiusChange: false,
+                  dir: CircularTextDirection.clockwise,
+                ),
               ),
+            ),
+          ),
+        ),
+        Center(
+          child: RotationTransition(
+            turns:
+                Tween(begin: 1.0, end: 0.0).animate(_rotationControllerSlower),
+            child: BuildCircularText(
+              dir: CircularTextDirection.anticlockwise,
+              radiusChange: true,
             ),
           ),
         ),
       ],
     );
+  }
+}
+
+class BuildCircularText extends StatelessWidget {
+  final bool radiusChange;
+  final CircularTextDirection dir;
+
+  const BuildCircularText({
+    Key key,
+    this.dir,
+    this.radiusChange,
+  }) : super(key: key);
+
+  List<TextItem> _recordText(BuildContext context) {
+    List<TextItem> _list = new List<TextItem>();
+    for (var i = 0; i < 6; i++) {
+      _list.add(
+        TextItem(
+          text: Text(
+            i % 2 == 0 ? "RECORD".toUpperCase() : "•".toUpperCase(),
+            style: ThemeProvider.themeOf(context)
+                .data
+                .primaryTextTheme
+                .headline6
+                .copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: ThemeProvider.themeOf(context).id == "light_theme"
+                      ? Colors.grey[400]
+                      : Colors.grey[850],
+                ),
+          ),
+          space: 16,
+          startAngle: i * 60.0,
+          startAngleAlignment: StartAngleAlignment.center,
+          direction: dir,
+        ),
+      );
+    }
+    return _list;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return radiusChange
+        ? Transform.rotate(
+            angle: -pi / 3,
+            child: CircularText(
+              children: _recordText(context),
+            ),
+          )
+        : CircularText(
+            children: _recordText(context),
+          );
   }
 }
 
